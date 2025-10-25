5b98083b87cd2f62ec2332953df303a6843743ee
CrazyProger1 <crazyproger1@gmail.com>
2025-10-25 22:31:04 +0300

    feat: added ultimate ruler tool

--- src/components/modules/map/tools/RulerTool.tsx
+++ src/components/modules/map/tools/RulerTool.tsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { LatLng } from "leaflet";
+import { Marker, Polyline, Popup, Tooltip, useMapEvents } from "react-leaflet";
 
 export const RulerTool = () => {
-  const [points, setPoints] = React.useState<LatLng[]>([]);
+  const [points, setPoints] = useState<LatLng[]>([]);
 
-  return <div></div>;
+  useMapEvents({
+    click: (event) => {
+      setPoints((prev) => [...prev, event.latlng]);
+    },
+  });
+
+  const distances = useMemo(() => {
+    let distance = 0;
+    let prevPoint: LatLng;
+    return points.map((point) => {
+      if (prevPoint) {
+        distance += prevPoint.distanceTo(point);
+      }
+      prevPoint = point;
+      return distance;
+    });
+  }, [points]);
+
+  const getDistanceLabel = (distance: number) => {
+    return distance > 1000 ? (distance / 1000).toFixed(2) + " km" : distance.toFixed(0) + " m";
+  };
+
+  return (
+    <div>
+      {points.map((point, index) => (
+        <Marker
+          eventHandlers={{
+            dragend: (event) => {
+              const endPosition = event.target.getLatLng();
+              const newPoints = [...points];
+              newPoints[index] = endPosition;
+              setPoints(newPoints);
+            },
+          }}
+          draggable={true}
+          position={point}
+          key={index}
+        >
+          <Tooltip direction="bottom" offset={[-15, 30]} opacity={1} permanent>
+            {getDistanceLabel(distances[index])}
+          </Tooltip>
+        </Marker>
+      ))}
+      <Polyline positions={points} />
+    </div>
+  );
 };
