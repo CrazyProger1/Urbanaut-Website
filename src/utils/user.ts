912faf317f6ac806fb1c7522002d6c3f817fc211
Nikolay <crazyproger1@gmail.com>
2026-01-27 09:32:43 +0000

    fix: getUserFullName

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,5 +1,5 @@
 import { User } from "@/types";
 
 export const getUserFullName = (user: User) => {
-  return `${user.first_name}${user.last_name ? " " + user.last_name : ""}`;
+  return `${user.first_name ? user.first_name : ""}${user.last_name ? " " + user.last_name : ""}`;
 };
