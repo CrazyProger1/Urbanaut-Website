ead20ff5cc990e858b0735a9ba08eb70624baf17
Nikolay <crazyproger1@gmail.com>
2026-01-30 22:44:43 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,16 @@ const nextConfig: NextConfig = {
     remotePatterns: [new URL("http://localhost:8001/api/v1/files/**")],
   },
   // output: "standalone",
-  serverActions: {
-    allowedOrigins: ["urbanaut.club", "www.urbanaut.club", "localhost:3000"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "urbanaut.club",
+        "*.urbanaut.club", // wildcard may work partially; test with explicit ones first
+        "www.urbanaut.club", // if you use/redirect to www
+        "localhost:4000", // useful for local + proxy testing
+        "127.0.0.1:4000",
+      ],
+    },
   },
 };
 
